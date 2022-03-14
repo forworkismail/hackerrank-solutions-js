717885da9f3afb5b7d60d8bbc04a00c94f998bd7
Ismail <forwork.ismail@gmail.com>
2022-03-14 10:25:19 +0430

    Solve 23. Cats And A Mouse

---
+++ 2.Problem-Solving/23.CatsAndAMouse.js
@@ -0,0 +1,10 @@
+function catAndMouse(x, y, z) {
+  if (Math.abs(z - x) < Math.abs(z - y)) {
+      return 'Cat A'
+  } else if (Math.abs(z - x) > Math.abs(z - y)) {
+      return 'Cat B'
+  } else {
+      return 'Mouse C'
+  }
+
+}
