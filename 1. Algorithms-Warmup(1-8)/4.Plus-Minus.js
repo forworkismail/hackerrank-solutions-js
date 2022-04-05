975a4107a5e924c5d8dfe39e8b7b7d77313d5e3c
Ismail <forwork.ismail@gmail.com>
2022-04-05 22:37:40 +0430

    Refactor 4.Plus Minus

--- 1. Algorithms-Warmup(1-8)/4.Plus-Minus.js
+++ 1. Algorithms-Warmup(1-8)/4.Plus-Minus.js
@@ -1,20 +1,22 @@
-const array = [1, 1, 0, -1, -1];
-
 function plusMinus(arr) {
+  // Write your code here
   let positive = 0;
   let negative = 0;
-  let zeros = 0;
-
+  let zero = 0;
+  let count = arr.length;
+  
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      positive++;
-    } else if (arr[i] < 0) {
-      negative++;
-    } else {
-      zeros++;
-    }
+      if (arr[i] > 0) {
+          positive++;
+      } else if (arr[i] < 0) {
+          negative++;
+      } else {
+          zero++;
+      }
   }
-  console.log(`${(positive / arr.length).toFixed(6)}\n${(negative / arr.length).toFixed(6)}\n${(zeros / arr.length).toFixed(6)}`);
-}
+  
+  console.log((positive / count).toFixed(6));
+  console.log((negative / count).toFixed(6));
+  console.log((zero / count).toFixed(6));
 
-console.log(plusMinus(array));
+}
